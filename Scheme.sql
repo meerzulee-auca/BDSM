abc5d2125d2f3a40b6ea6816da9d7ead5d2e8fec
Meerzulee <meerzulee@gmail.com>
2020-12-11 12:43:20 +0600

    Add working rooms

--- Scheme.sql
+++ Scheme.sql
@@ -7,7 +7,7 @@ CREATE TABLE Building (
     TotalArea INT NOT NULL,
 );
 
--- GRAY  Floors of the building
+-- GRAY Floors of the building
 CREATE TABLE Floors (
     ID BIGINT NOT NULL CONSTRAINT PK_Floors PRIMARY KEY CLUSTERED,
     BuildingID VARCHAR(100) NOT NULL CONSTRAINT FK_Building FOREIGN KEY REFERENCES Building(Address),
@@ -15,4 +15,14 @@ CREATE TABLE Floors (
     UsableArea FLOAT NOT NULL,
     Operating BIT NOT NULL,
     CostPerSquare MONEY NOT NULL
+);
+
+-- Gray Working rooms
+CREATE TABLE WorkingRoom (
+    ID BIGINT NOT NULL CONSTRAINT PK_WorkingRoom PRIMARY KEY CLUSTERED,
+    FloorID bigint NOT NULL CONSTRAINT FK_Floors FOREIGN KEY REFERENCES Floors(ID),
+    RoomTitle VARCHAR(100),
+    Area FLOAT NOT NULL,
+    IsOccupated BIT NOT NULL,
+    MaxHumanLoad Int
 );
