a0bc6a67ad37f80e052372ae0aa310845757ce46
Meerzulee <meerzulee@gmail.com>
2020-12-11 12:27:14 +0600

    Create Scheme.sql

---
+++ Scheme.sql
@@ -0,0 +1,18 @@
+-- GRAY Buildings
+CREATE TABLE Building (
+    Address VARCHAR(100) NOT NULL CONSTRAINT PK_Building PRIMARY KEY CLUSTERED,
+    Operating BOOLEAN NOT NULL,
+    Title VARCHAR(32) NOT NULL,
+    NumberOfFloors INT NOT NULL,
+    TotalArea INT NOT NULL,
+);
+
+-- GRAY  Floors of the building
+CREATE TABLE Floors (
+    ID BIGINT NOT NULL CONSTRAINT PK_Floors PRIMARY KEY CLUSTERED,
+    BuildingID VARCHAR(100) NOT NULL CONSTRAINT FK_Building FOREIGN KEY REFERENCES Building(Address),
+    LEVEL INT NOT NULL,
+    UsableArea DOUBLE NOT NULL,
+    Operating BOOLEAN NOT NULL,
+    CostPerSquare MONEY NOT NULL
+);
