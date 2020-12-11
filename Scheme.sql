4f8bf7c140d253ae52307a9f755b29d0cc81a9bd
Talkanbaev Artur <talkanbaev.artur@outlook.com>
2020-12-11 12:43:46 +0600

    Company_v0.1

--- Scheme.sql
+++ Scheme.sql
@@ -1,14 +1,19 @@
+---- __________GRAY TABLES__________ ----
+
 -- GRAY Buildings
-CREATE TABLE Building (
+CREATE TABLE Building
+(
     Address VARCHAR(100) NOT NULL CONSTRAINT PK_Building PRIMARY KEY CLUSTERED,
     Operating BOOLEAN NOT NULL,
     Title VARCHAR(32) NOT NULL,
     NumberOfFloors INT NOT NULL,
     TotalArea INT NOT NULL,
 );
+GO
 
 -- GRAY  Floors of the building
-CREATE TABLE Floors (
+CREATE TABLE Floors
+(
     ID BIGINT NOT NULL CONSTRAINT PK_Floors PRIMARY KEY CLUSTERED,
     BuildingID VARCHAR(100) NOT NULL CONSTRAINT FK_Building FOREIGN KEY REFERENCES Building(Address),
     LEVEL INT NOT NULL,
@@ -16,3 +21,22 @@ CREATE TABLE Floors (
     Operating BOOLEAN NOT NULL,
     CostPerSquare MONEY NOT NULL
 );
+GO
+
+---- __________RED TABLES__________ ----
+
+
+-- Create a new table called 'Companies' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Companies', 'U') IS NOT NULL
+DROP TABLE dbo.Companies
+GO
+-- Create the table in the specified schema
+CREATE TABLE dbo.Companies
+(
+    ID INT NOT NULL CONSTRAINT PK_Companies PRIMARY KEY,-- primary key column
+    Title [NVARCHAR] (50) NOT NULL,
+    Capitalisation bigint NOT NULL
+    -- specify more columns here
+);
+GO
