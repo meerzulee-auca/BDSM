85bb98d42a23543dcd26e5c62cf36ee26c6e5892
Talkanbaev Artur <talkanbaev.artur@outlook.com>
2020-12-11 12:58:06 +0600

    Companies_v0.2

--- Scheme.sql
+++ Scheme.sql
@@ -1,6 +1,7 @@
 ---- __________GRAY TABLES__________ ----
 -- GRAY Buildings
-CREATE TABLE dbo.Building (
+CREATE TABLE dbo.Building
+(
     Address VARCHAR(100) NOT NULL CONSTRAINT PK_Building PRIMARY KEY CLUSTERED,
     Operating BIT NOT NULL,
     Title VARCHAR(32) NOT NULL,
@@ -9,39 +10,45 @@ CREATE TABLE dbo.Building (
 );
 
 GO
-    -- GRAY Floors of the building
-    CREATE TABLE dbo.Floors (
-        ID BIGINT NOT NULL CONSTRAINT PK_Floors PRIMARY KEY CLUSTERED,
-        BuildingID VARCHAR(100) NOT NULL CONSTRAINT FK_Building FOREIGN KEY REFERENCES Building(Address),
-        LEVEL INT NOT NULL,
-        UsableArea FLOAT NOT NULL,
-        Operating BIT NOT NULL,
-        CostPerSquare MONEY NOT NULL
-    );
+-- GRAY Floors of the building
+CREATE TABLE dbo.Floors
+(
+    ID BIGINT NOT NULL CONSTRAINT PK_Floors PRIMARY KEY CLUSTERED,
+    BuildingID VARCHAR(100) NOT NULL CONSTRAINT FK_Building FOREIGN KEY REFERENCES Building(Address),
+    LEVEL INT NOT NULL,
+    UsableArea FLOAT NOT NULL,
+    Operating BIT NOT NULL,
+    CostPerSquare MONEY NOT NULL
+);
 
 GO
-    -- Gray Working rooms
-    CREATE TABLE dbo.WorkingRoom (
-        ID BIGINT NOT NULL CONSTRAINT PK_WorkingRoom PRIMARY KEY CLUSTERED,
-        FloorID bigint NOT NULL CONSTRAINT FK_Floors FOREIGN KEY REFERENCES Floors(ID),
-        RoomTitle VARCHAR(100),
-        Area FLOAT NOT NULL,
-        IsOccupated BIT NOT NULL,
-        MaxHumanLoad INT
-    );
+-- Gray Working rooms
+CREATE TABLE dbo.WorkingRoom
+(
+    ID BIGINT NOT NULL CONSTRAINT PK_WorkingRoom PRIMARY KEY CLUSTERED,
+    FloorID bigint NOT NULL CONSTRAINT FK_Floors FOREIGN KEY REFERENCES Floors(ID),
+    RoomTitle VARCHAR(100),
+    Area FLOAT NOT NULL,
+    IsOccupated BIT NOT NULL,
+    MaxHumanLoad INT
+);
 
 GO
-    ---- __________RED TABLES__________ ----
-    -- Create a new table called 'Companies' in schema 'dbo'
-    -- Drop the table if it already exists
-    IF OBJECT_ID('dbo.Companies', 'U') IS NOT NULL DROP TABLE dbo.Companies
+---- __________RED TABLES__________ ----
+-- Create a new table called 'Companies' in schema 'dbo'
+-- Drop the table if it already exists
+IF OBJECT_ID('dbo.Companies', 'U') IS NOT NULL DROP TABLE dbo.Companies
 GO
-    -- Create the table in the specified schema
-    CREATE TABLE dbo.Companies (
-        ID INT NOT NULL CONSTRAINT PK_Companies PRIMARY KEY,
-        -- primary key column
-        Title [NVARCHAR] (50) NOT NULL,
-        Capitalisation bigint NOT NULL -- specify more columns here
-    );
+-- Create the table in the specified schema
+CREATE TABLE dbo.Companies
+(
+    ID INT NOT NULL CONSTRAINT PK_Companies PRIMARY KEY,
+    -- primary key column
+    Title [NVARCHAR] (50) NOT NULL,
+    Capitalisation bigint NOT NULL,
+    Priority int NOT NULL,
+    PotfolioURL varchar(400),
+    PhoneNumberOfResponsible VARCHAR(50) NOT NULL
+);
 
 GO
