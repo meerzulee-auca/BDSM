1ad095dc198e3d0dfc6f7fa6b41311f71381990d
Meerzulee <meerzulee@gmail.com>
2020-12-11 12:34:14 +0600

    fixing datetypes

--- Scheme.sql
+++ Scheme.sql
@@ -1,7 +1,7 @@
 -- GRAY Buildings
 CREATE TABLE Building (
     Address VARCHAR(100) NOT NULL CONSTRAINT PK_Building PRIMARY KEY CLUSTERED,
-    Operating BOOLEAN NOT NULL,
+    Operating BIT NOT NULL,
     Title VARCHAR(32) NOT NULL,
     NumberOfFloors INT NOT NULL,
     TotalArea INT NOT NULL,
@@ -12,7 +12,7 @@ CREATE TABLE Floors (
     ID BIGINT NOT NULL CONSTRAINT PK_Floors PRIMARY KEY CLUSTERED,
     BuildingID VARCHAR(100) NOT NULL CONSTRAINT FK_Building FOREIGN KEY REFERENCES Building(Address),
     LEVEL INT NOT NULL,
-    UsableArea DOUBLE NOT NULL,
-    Operating BOOLEAN NOT NULL,
+    UsableArea FLOAT NOT NULL,
+    Operating BIT NOT NULL,
     CostPerSquare MONEY NOT NULL
 );
